c644f60dc5846ca88573ba8388aebc6b01f949b8
furl726 <incredibleayush81@gmail.com>
2020-03-06 17:06:13 +0530

    done changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyBark5MFyEOINVnPiYoEmE4BAUvbS-Vvuk';
 
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {videos:[]};
+    YTSearch({key:API_KEY, term:'Sachin Tendulkar'},
+
+        (videos)=>{
+        this.setState({videos});     // ES6 replacement of {vedios:vedios}
+        });
+    }
+    render(){
+        return(
+          <div>
+              <SearchBar />
+          </div>
+        );
+    }
+}
