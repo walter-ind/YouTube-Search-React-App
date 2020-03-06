268900eaf4c998f1843b9b9db65444b9c273dd68
furl726 <incredibleayush81@gmail.com>
2020-03-06 18:05:51 +0530

    done changes

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from "./components/video_list";
 const API_KEY = 'AIzaSyBark5MFyEOINVnPiYoEmE4BAUvbS-Vvuk';
 
 class App extends Component{
     constructor(props) {
         super(props);
         this.state = {videos:[]};
-    YTSearch({key:API_KEY, term:'Sachin Tendulkar'},
+    YTSearch({key:API_KEY, term:'surfboards'},
 
         (videos)=>{
-        console.log(videos);
         this.setState({videos});     // ES6 replacement of {vedios:vedios}
         });
     }
@@ -19,6 +19,7 @@ class App extends Component{
         return(
           <div>
               <SearchBar />
+              <VideoList videos={this.state.videos} />
           </div>
         );
     }
