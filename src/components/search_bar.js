e3b030ba070dd43dd3e3c7b42bd03345d23b21fd
furl726 <incredibleayush81@gmail.com>
2020-04-10 23:42:31 +0530

    added Search Bar file

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,20 +3,23 @@ import React,{Component} from 'react';
 class SearchBar extends Component{
     constructor(props){
         super(props);
-        this.state = {term:'Enter the term'};    // state property of class based component works as placeholder => a benefit of 'state' //
+        this.state = {term:''};    // state property of class based component works as placeholder => a benefit of 'state' //
     }
 
     render(){
         return(
-            <div>
+            <div className="search-bar">
                <input
                 value = {this.state.term}
-               onChange = {(event)=>{this.setState({term:event.target.value})}}
+                onChange = {event => this.onInputChange(event.target.value)}
                />
             </div>
         );
     }
-
+    onInputChange(term){
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
 }
 
 export default SearchBar;
