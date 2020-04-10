69649dacd452a581e7ae2f9e7a2d1c87d40bfb04
furl726 <incredibleayush81@gmail.com>
2020-04-10 22:54:06 +0530

    done changes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
 const API_KEY = 'AIzaSyBark5MFyEOINVnPiYoEmE4BAUvbS-Vvuk';
 
 class App extends Component{
     constructor(props) {
         super(props);
-        this.state = {videos:[]};
-    YTSearch({key:API_KEY, term:'surfboards'},
-
-        (videos)=>{
-        this.setState({videos});     // ES6 replacement of {vedios:vedios}
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards');
+    }
+    videoSearch(term){
+    YTSearch({key:API_KEY, term:term}, (videos)=>{
+        this.setState({
+            videos:videos,
+            selectedVideo:videos[0]
         });
+      });
     }
     render(){
         return(
