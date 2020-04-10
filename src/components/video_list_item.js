8841bc23b9a1d3f607f7f7c9bbf40bf78661af2a
furl726 <incredibleayush81@gmail.com>
2020-04-11 01:05:00 +0530

    added Video List item file

---
+++ src/components/video_list_item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const VideoListItem = ({video})=>{
+    const imageUrl = video.snippet.thumbnail.default.url;
+    return (
+      <li className="list-group-item">
+          <div className="video-list media">
+              <div className="media-left">
+                  <img className="media-object" src="{imageUrl}"/>
+              </div>
+              <div className="media-body">
+                  <div className="media-heading">{video.snippet.title}</div>
+              </div>
+          </div>
+      </li>
+    );
+};
+export default videoListItem;
