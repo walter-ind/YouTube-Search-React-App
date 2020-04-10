93cddf6509e316d7ef6a701f3ce6a242ff29af78
furl726 <incredibleayush81@gmail.com>
2020-04-10 23:58:42 +0530

    added Video List file

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import VideoListitem from './video_list_item';
 
 const VideoList = (props)=>{
+    const videoItems = props.videos.map((video)=>{
+        return(
+        <VideoListItem
+            onVideoSelect = {props.onVideoSelect}
+            key={video.etag}
+            video={video}
+         />
+        );
+    });
     return(
         <ul ClassName = 'col-md-4 list-group'>
-            {props.videos.length};
+            {videoItems}
         </ul>
-
-);
+  );
 };
 
 export default VideoList;
